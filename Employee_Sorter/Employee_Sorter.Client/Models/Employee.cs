d7cb92fd2bca6538cf15fb55d3d7117a561b96d4
Ivan Tzenev <ivan.tzenev@semansys.com>
2025-10-01 16:10:59 +0300

    Add project files.

---
+++ Employee_Sorter/Employee_Sorter.Client/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿namespace Employee_Sorter.Client.Models
+{
+    public class Employee
+    {
+        public int EmpID { get; set; }
+
+        public int ProjectID { get; set; }
+
+        public DateOnly DateFrom { get; set; }
+
+        public DateOnly? DateTo { get; set; }
+
+        public int DaysWorked => GetDaysWorked();
+
+        private int GetDaysWorked()
+        {
+            DateOnly endDate = DateTo ?? DateOnly.FromDateTime(DateTime.Now);
+            return (endDate.ToDateTime(new TimeOnly()) - DateFrom.ToDateTime(new TimeOnly())).Days;
+        }
+    }
+
+    public class EmployeePair
+    {
+        public int EmpID1 { get; set; }
+
+        public int EmpID2 { get; set; }
+
+        public int ProjectID { get; set; }
+
+        public int DaysWorkedTogether { get; set; }
+    }
+}
